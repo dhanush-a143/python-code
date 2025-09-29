00d0cfd96a8eba0d9b2be653ba65acdb5421a2e7
dhanush-a <dhanush7772k@gmail.com>
2025-09-29 15:14:56 +0530

    python

---
+++ day 2.py
@@ -0,0 +1,61 @@
+# casting
+
+x="10"
+y="60"
+z="10.5"
+
+a=int(x)
+
+print(type(x))
+print(type(y))
+
+print(type(float(z)))
+
+print(type(a))
+
+
+# ....
+
+a="40"
+b="50"
+
+print(int(a)+int(b))
+
+# concadination
+
+a="50"
+b="5"
+
+print(a+b)
+
+# OPERATORS
+
+a=10
+b=10
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# floar
+print(a//b)
+
+# comparison
+
+a=3
+b=7
+print(a==b)
+print(a!=b)
+
+# boolian
+
+d=True
+a=False
+
+print(d and a)
+print(d or a)
+
+print(a+b)
